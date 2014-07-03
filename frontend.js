c59c1b6a8d0b34df90a73d272ec564ff23a262db
Nikita Zaharov aka ix <ix@2du.ru>
2014-07-03 22:44:03 +0600

    adding skeleton of reimplementation frontend on ui objecdts

--- frontend.js
+++ frontend.js
@@ -2,86 +2,39 @@
  * Сервис, который реализует б'ольшую часть функциональности сферы, конечно же используя другие сервисы.
  * Может быть как частью приложения, так и работать отдельно.
  * Его назначение - сосредоточить в себе функциональность сферы полностью абстрагировавшись от способов
- * вывода информации, способов её передачи, хранения и извлечения. По этим причинам, frontend использует ui
+ * вывода информации, способов её передачи и хранения. По этим причинам, frontend использует ui
  * как frontend себя и backend для хранения и переработки совместно с другими frontend.
  */
 
+var ui = require('../dsa/objects/ui.js');
+
+function open_addr(stack){
+    
+}
+
+function address_requester(stack){
+    with(ui.highlevel){
+	new card({ name : 'address' }, null, stack);
+	new entry({ name : 'addr',
+		    height : 1,
+		    width : 3,
+		    advertisement : 'введите адрес'
+		  }, null, stack);
+	new click({ height : 1,
+		    width : 2,
+		    label : 'открыть',
+		    on_press : open_addr
+		  }, null, stack);
+    }
+}
+
 exports.init = function(env, dsa){
-    var ui = env.dsa.objects.ui;
     var _backend;
     dsa.on('create', 
 	   function(sprout, stack, backend){
 	       _backend = backend;
 	       ui.init('pc');
-	       ui.block_size_ask();
-/*	       
- 		       msg(ui, 'card_create', {
-			       name : 'address'
-			   }, 'main').sprout(
-			       msg(ui, 'part_create', {
-				       type : 'text_input',
-				       height : 1,
-				       width : 5,
-				       advetisement : 'введите адрес'
-				   }),
-			       msg(ui, 'part_create', {
-				       type : 'click_item',
-				       height : 1,
-				       width : 2,
-				       label : 'открыть',
-				       on_pressed : [f(function(sprout, stack){
-							   console.log('go go go');       
-						       }),
-						     msg(ui, 'card_create', {
-							     name : 'actions'
-							 }, 'main').sprout(
-							     msg(ui, 'part_create', {
-								     type : 'click_item',
-								     height : 1,
-								     width : 2,
-								     label : 'edit'
-								 }),
-							     msg(ui, 'part_create', {
-								     type : 'click_item',
-								     row : true,
-								     height : 1,
-								     width : 2,
-								     label : 'create'
-								 })
-							 )
-						    ]
-				   }),
-			       msg(ui, 'part_create', {
-				       type : 'click_item',
-				       height : 1,
-				       width : 2,
-				       label : 'новый',
-				       on_pressed : [f(function(sprout, stack){
-							   console.log('go go go');       
-						       }),
-						     msg(ui, 'card_create', {
-							     name : 'actions'
-							 }, 'main').sprout(
-							     msg(ui, 'part_create', {
-								     type : 'click_item',
-								     height : 1,
-								     width : 2,
-								     label : 'edit'
-								 }),
-							     msg(ui, 'part_create', {
-								     type : 'click_item',
-								     row : true,
-								     height : 1,
-								     width : 2,
-								     label : 'create'
-								 })
-							 )
-						    ]
-				   })
-			   )
-		      ).run(stack);
- 
-	   }
+	       ui.block_size_ask(address_requester);
 
 //    var address_panel = sloader.load('sphere/ui/address_panel', mq, env);
 //    var action_panel = sloader.load('sphere/ui/action_panel', mq, env);
@@ -111,4 +64,4 @@ exports.init = function(env, dsa){
     dsa.on('destroy',
 	  function(sprout, stack){
 	  });
-}
+};
